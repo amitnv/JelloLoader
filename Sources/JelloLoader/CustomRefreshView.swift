923fb4366ff37aa21a33b309cf9004cff7fa9c9b
Amit Vaidya <amitvaidhya@gmail.com>
2023-06-14 23:16:04 +0100

    Re-added dynamic island to viewModel

--- Sources/JelloLoader/CustomRefreshView.swift
+++ Sources/JelloLoader/CustomRefreshView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import DeviceGuru
 
 // MARK: Custom View Builder
 @available(iOS 15.0, *)
@@ -15,12 +14,10 @@ public struct CustomRefreshView<Content: View>: View {
     public var content: Content
     // MARK: Async Call Back
     public var onRefresh: ()async->()
-    var hasDynamicIsland: Bool = false
     public init(@ViewBuilder content: @escaping ()->Content,
          onRefresh: @escaping ()async->()) {
         self.content = content()
         self.onRefresh = onRefresh
-        checkForDynamicIsland()
     }
     
     public var body: some View {
@@ -56,8 +53,8 @@ public struct CustomRefreshView<Content: View>: View {
                 Capsule()
                     .fill(.black)
             }
-            .frame(width: 126, height: hasDynamicIsland ? 37 : 28)
-            .offset(y: hasDynamicIsland ? 11 : 0)
+            .frame(width: 126, height: scrollDelegate.hasDynamicIsland ? 37 : 28)
+            .offset(y: scrollDelegate.hasDynamicIsland ? 11 : 0)
             .frame(maxHeight: .infinity, alignment: .top)
             .overlay(alignment: .top, content: {
                 // MARK: For More See Shape Morphing And MetaBall Animations Video
@@ -72,7 +69,7 @@ public struct CustomRefreshView<Content: View>: View {
                                 // Dynamic Island Offset -> 11
                                 // Circle Radius -> 38/2 -> 19
                                 // Total -> 11 + 19 -> 30
-                                ctx.draw(resolvedView, at: CGPoint(x: size.width / 2, y: hasDynamicIsland ? 30 : 10))
+                                ctx.draw(resolvedView, at: CGPoint(x: size.width / 2, y: scrollDelegate.hasDynamicIsland ? 30 : 10))
                             }
                         }
                     }
@@ -89,7 +86,7 @@ public struct CustomRefreshView<Content: View>: View {
             })
             .overlay(alignment: .top, content: {
                 RefreshView()
-                    .offset(y: hasDynamicIsland ? 11 : -10)
+                    .offset(y: scrollDelegate.hasDynamicIsland ? 11 : -10)
             })
             .ignoresSafeArea()
         })
@@ -163,13 +160,6 @@ public struct CustomRefreshView<Content: View>: View {
         .animation(.easeInOut(duration: 0.25), value: scrollDelegate.isEligible)
         .opacity(scrollDelegate.progress)
         .offset(y: offset)
-    }
-    mutating func checkForDynamicIsland() {
-        let deviceGuru = DeviceGuruImplementation()
-        let deviceName = deviceGuru.hardwareString
-        if deviceName == "iPhone15,2" || deviceName == "iPhone15,3" {
-            hasDynamicIsland = true
-        }
     }
 }
 
--- Sources/JelloLoader/ScrollViewModel.swift
+++ Sources/JelloLoader/ScrollViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import DeviceGuru
 
 @available(iOS 15.0, *)
 class ScrollViewModel: NSObject, ObservableObject, UIGestureRecognizerDelegate {
@@ -17,6 +18,8 @@ class ScrollViewModel: NSObject, ObservableObject, UIGestureRecognizerDelegate {
     @Published var scrollOffset: CGFloat = 0
     @Published var contentOffset: CGFloat = 0
     @Published var progress: CGFloat = 0
+    @Published var hasDynamicIsland: Bool = false
+    
     private let gestureID = UUID().uuidString
     
     // MARK: Since We need to Know when the user Left the Screen to Start Refresh
@@ -28,8 +31,16 @@ class ScrollViewModel: NSObject, ObservableObject, UIGestureRecognizerDelegate {
         return true
     }
     
+    func checkForDynamicIsland() {
+        let deviceGuru = DeviceGuruImplementation()
+        let deviceName = deviceGuru.hardwareString
+        if deviceName == "iPhone15,2" || deviceName == "iPhone15,3" {
+            hasDynamicIsland = true
+        }
+    }
     // MARK: Adding gesture
     func addGesture() {
+        checkForDynamicIsland()
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(onGestureChange(gesture:)))
         panGesture.delegate = self
         panGesture.name = gestureID
